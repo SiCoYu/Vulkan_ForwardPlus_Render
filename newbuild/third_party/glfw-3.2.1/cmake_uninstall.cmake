4e8d75d9e8d72a75168b60f4434541be707fc91a
SiCoYu <yxk_neu2013@163.com>
2022-02-09 23:34:36 +0800

    更新解决方案sln

--- newbuild/third_party/glfw-3.2.1/cmake_uninstall.cmake
+++ newbuild/third_party/glfw-3.2.1/cmake_uninstall.cmake
@@ -1,9 +1,9 @@
 
-if (NOT EXISTS "E:/GitHub_WorkPlace/Vulkan-Forward-Plus-Renderer/newbuild/third_party/glfw-3.2.1/install_manifest.txt")
-  message(FATAL_ERROR "Cannot find install manifest: \"E:/GitHub_WorkPlace/Vulkan-Forward-Plus-Renderer/newbuild/third_party/glfw-3.2.1/install_manifest.txt\"")
+if (NOT EXISTS "E:/GitHub_WorkPlace/Vulkan_ForwardPlus_Render/newbuild/third_party/glfw-3.2.1/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"E:/GitHub_WorkPlace/Vulkan_ForwardPlus_Render/newbuild/third_party/glfw-3.2.1/install_manifest.txt\"")
 endif()
 
-file(READ "E:/GitHub_WorkPlace/Vulkan-Forward-Plus-Renderer/newbuild/third_party/glfw-3.2.1/install_manifest.txt" files)
+file(READ "E:/GitHub_WorkPlace/Vulkan_ForwardPlus_Render/newbuild/third_party/glfw-3.2.1/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
